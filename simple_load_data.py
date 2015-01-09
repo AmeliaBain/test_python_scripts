d5b096af3d3df9788681d469e4033a398c448bb1
simon-m-mudd <simon.m.mudd@ed.ac.uk>
2015-01-09 17:16:25 +0000

    Added the simple_load_data python scrript

---
+++ simple_load_data.py
@@ -0,0 +1,15 @@
+# This program was written by Simon
+
+# import the numerical python package
+import numpy as np
+
+print "Welcome to the load data file, I am your computer."
+print "I am going to try to open the file some_data.csv"
+my_data = np.loadtxt(fname = 'some_data.csv', delimiter=',')
+
+
+
+
+print "let me show you what I understood:"
+print my_data
+
