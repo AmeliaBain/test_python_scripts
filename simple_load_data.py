df176a53292059b0953765a5459d83fc6db49345
simon-m-mudd <simon.m.mudd@ed.ac.uk>
2015-01-09 17:34:42 +0000

    Committing recent script work

--- simple_load_data.py
+++ simple_load_data.py
@@ -5,7 +5,4 @@ import numpy as np
 
 print "Welcome to the load data file, I am your computer."
 print "I am going to try to open the file some_data.csv"
-my_data = np.loadtxt(fname = 'some_data.csv', delimiter=',')
-
-
-
+# my_data = np.loadtxt(fname = 'some_data.csv', delimiter=',')
