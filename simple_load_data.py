e122bf58b6167d8df890c02ef18e70dd2c7a586b
simon-m-mudd <simon.m.mudd@ed.ac.uk>
2015-01-13 14:56:25 +0000

    Added a few more files for loading data

--- simple_load_data.py
+++ simple_load_data.py
@@ -6,3 +6,25 @@ import numpy as np
 print "Welcome to the load data file, I am your computer."
 print "I am going to try to open the file some_data.csv"
 # my_data = np.loadtxt(fname = 'some_data.csv', delimiter=',')
+
+f = open('some_data.csv','r')
+
+print "What is f?"
+print f
+
+lines = f.readlines()
+
+print "these are the lines in the file"
+print lines
+
+line = lines[0]
+
+split_line = lines[0].split(',')
+print "split line is: "
+print split_line
+
+for element in split_line:
+  num = int(element)
+  print num
+
+
---
+++ simple_load_data3.py
@@ -0,0 +1,44 @@
+# This program was written by Simon M. Mudd
+# A simple progrtam for reading csv files
+
+# import the numerical python package
+import numpy as np
+
+print "Welcome to the load data file, I am your computer."
+print "I am going to try to open the file some_data.csv"
+# my_data = np.loadtxt(fname = 'some_data.csv', delimiter=',')
+
+# here is where you tell python what the filename is
+f = open('some_data.csv','r')
+
+print "What is f?"
+print f
+
+lines = f.readlines()
+
+print "these are the lines in the file"
+print lines
+
+line = lines[0]
+
+split_line = lines[0].split(',')
+print "split line is: "
+print split_line
+
+
+# find the number of elements in the line
+n_elements_in_line = len(split_line)
+
+
+# start an empty list. 
+# what happens if you don't start with an empty list?
+line_nums = []
+
+# look through the line, getting the elements
+for element in split_line:
+  num = int(element)
+  print num
+  line_nums.append(num)
+  
+# the line numbers are:  
+print line_nums
